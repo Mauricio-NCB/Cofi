7b73fa14cd9b2410e1e4e5ba1474864f5596de9c
Mauricio-NCB <mncalderon2001@gmail.com>
2026-03-12 20:58:58 +0100

    Ahora sí Modal de Chat

--- src/main/java/com/website/main/controller/ChatController.java
+++ src/main/java/com/website/main/controller/ChatController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
+import com.website.main.model.Chat;
+import com.website.main.model.Message;
 import com.website.main.service.ChatService;
 import com.website.main.service.MessageService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 @Controller
@@ -37,10 +41,15 @@ public class ChatController {
     @GetMapping("/{chatId}")
     public String viewChat(@PathVariable Integer chatId, Model model) {
         // Aquí iría la lógica para obtener el chat y sus mensajes
-        // model.addAttribute("chat", chat);
-        // model.addAttribute("messages", messages);
-        
-        return "chat/view";
+        Chat chat = chatService.findById(chatId);
+        List<Message> messages = messageService.viewMessagesFromChat(chatId);
+
+        model.addAttribute("chat", chat);
+        model.addAttribute("messages", messages);
+        model.addAttribute("username", "Mauricio");
+        model.addAttribute("currentPage", "chat");
+
+        return "chat";
     }
     
     // Implementacion para enviar mensajes via JS
@@ -52,6 +61,6 @@ public class ChatController {
         // chatService.sendMessage(chatId, content);
         messageService.sendMessage(chatId, content, userId);
 
-        return "redirect:/chats/" + chatId;
+        return "redirect:/chat/" + chatId;
     }
 }
